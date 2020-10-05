6d1ab42266f52f8ca04744af01a8f23b550e56bf
Alicia S. Johnson <alicia.s.johnson@huntington.com>
2020-10-05 00:57:17 -0400

    adding express framework that will be used to display front end and backend for app

--- server.js
+++ server.js
@@ -1,14 +1,53 @@
 var http = require('http');
+var fs = require('fs');
 
-function onRequest(request, response){
-	console.log("A user made a request" + request.url);
-	response.writeHead(200, {"Context-Type": "text/plain"});
-	response.write("Here is some data");
-	response.end();
-}
+var connect = require('connect');
 
-http.createServer(onRequest).listen(8080);
+var app = connect();
+
+function homePage(request, response){
+	console.log("User requested home page")
+};
+
+function myHealth(request, response){
+	console.log("User requested My Health Page")
+};
+
+function about(request, response){
+	console.log("User requested About Page")
+};
+
+app.use('/', homePage);
+app.use('/index', homePage);
+app.use('/my_health', myHealth);
+app.use('/about', about);
+
+http.createServer(app).listen(8080);
 console.log("Server is now running...");
+
+
+//old code(
+//404 response
+//function send404Response(response){
+//	response.writeHead(404, {"Content-Type": "text/plain"});
+//	response.write("Error 404: Page Not Found");
+//	response.end();
+//}
+//Handle a user request. Send to home page
+//function onRequest(request, response){
+//	if(request.method == 'GET' && request.url == '/'){
+//		response.writeHead(200, {"Content-Type": "text/html"});
+//		fs.createReadStream("index.html").pipe(response);
+//	}else{
+//		send404Response(response)
+//	}
+//		
+//}
+//
+//http.createServer(onRequest).listen(8080);
+//console.log("Server is now running...");
+//
+//)
 
 
 
